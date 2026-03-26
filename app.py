29760d3329bbf078a7625ff16ffef57519271dcd
AlexanderDonchev2011 <alexanderdonchev11@gmail.com>
2026-03-26 11:22:49 +0200

    gemini link to code

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from google import genai
 
+KEY = "AIzaSyAYA7JNLpiEoNRUEuSRJL7z6HslMv4-4A4" 
+
+client = genai.Client(api_key=KEY)
+
+try:
+    user_input = input("Enter your prompt: ")
+    
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=user_input
+    )
+    print(response.text)
+except Exception as e:
+    print(f"Error occurred: {e}")
